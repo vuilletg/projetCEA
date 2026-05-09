f43884fc7ee54d42721dbdb77ad97c1a543fae11
Nguyễn Duy Thắng <nguyenduythang@Mac.lan>
2026-05-09 23:22:44 +0200

    Ajout de l'option Boxplot avec boutons radio dans la section Critères

--- read_greenH2_dataset.py
+++ read_greenH2_dataset.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import plotly.graph_objects as go
+import plotly.express as px  # NOUVELLE: Utilise pour Boxplot 
 from dash import Dash, dcc, html, Input, Output, State
 import os
 
@@ -36,6 +37,34 @@ app.layout = html.Div([
 
     html.Div(id='graphs-container')
 ])
+
+# CREATION BOXPLOT
+def create_boxplot(y_col):
+    if y_col is None or df is None:
+        return go.Figure()
+    
+    clean_label = y_col.split('_')[-1]
+
+    fig = px.box(
+        df,
+        x = "Technology",
+        y = y_col,
+        color = "Technology",
+        facet_col = "Year",
+        points = "outliers",
+        title = f"Distribution de : {clean_label}",
+        labels={
+            "Technology": "Technologie",
+            "Year": "Année",
+            y_col: clean_label
+        },
+        template="plotly_white"
+    )
+    fig.update_layout(transition_duration=300,
+                      margin = dict(l=40, r=40, t=80, b=40),
+                      legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1)
+                      )
+    return fig
 
 @app.callback(
     Output('graphs-container', 'children'),
@@ -87,6 +116,19 @@ def update_all_data(n_clicks, filename):
         ]),
         html.Div([
             html.H2("Critères"),
+
+            # Ajouter RADIO BUTTON
+            dcc.RadioItems(
+                id='crit-plot-mode',
+                options=[
+                    {'label': ' Standard (Scatter)', 'value': 'std'},
+                    {'label': ' Boxplot (Distribution)', 'value': 'box'}
+                ],
+                value='std',
+                labelStyle={'display': 'inline-block', 'marginRight': '20px', 'fontWeight': 'bold'}
+            ),
+            #### FIN ####
+
             dcc.Dropdown(id='x-crit', options=criteria_options, value=criteria_dispo[0]),
             dcc.Dropdown(id='y-crit', options=criteria_options, value=criteria_dispo[1]),
             dcc.Graph(id='crit_plot')
@@ -141,8 +183,14 @@ def update_t(x, y, z, yr): return create_fig(x, y, z, yr)
 @app.callback(Output('cont_plot', 'figure'), [Input('x-cont', 'value'), Input('y-cont', 'value'), Input('year-slider-cont', 'value')])
 def update_co(x, y, yr): return create_fig(x, y, year_idx=yr)
 
-@app.callback(Output('crit_plot', 'figure'), [Input('x-crit', 'value'), Input('y-crit', 'value')])
-def update_cr(x, y): return create_fig(x, y)
+@app.callback(Output('crit_plot', 'figure'), [Input('x-crit', 'value'), Input('y-crit', 'value'),
+                                              Input('crit-plot-mode', 'value') # Ajouter une nouvelle INPUT à partir "RADIO BUTTON"
+                                              ])
+#UPDATE
+def update_cr(x, y, mode): 
+    if mode == 'box':
+        return create_boxplot(y)
+    return create_fig(x, y)
 
 if __name__ == '__main__':
     app.run(debug=True)
